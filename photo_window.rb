83cf82a9134027ecfa2d37e22c212d7720f909f9
Tom May <tom@tommay.net>
2017-02-08 23:49:45 -0800

    A lot of stuff almost works.

--- photo_window.rb
+++ photo_window.rb
@@ -1,6 +1,8 @@
 require "gtk3"
 
 class PhotoWindow
+  FRAME_PIXELS = 4
+
   def initialize
     # @image is a Gtk::DrawingArea used for getting the on-screen size
     # and for displaying a framed/cropped pixbuf with cairo.  The
@@ -36,22 +38,37 @@ class PhotoWindow
   end
 
   def set_aspect(width, height)
-    puts "aspect #{width} #{height}"
+    @crop = @crop.set_aspect(width, height)
+    prepare_pixbuf
+    @image.queue_draw
   end
 
   def show_photo(filename)
     @pixbuf = filename && GdkPixbuf::Pixbuf.new(file: filename)
+    @crop = Crop.for_pixbuf(@pixbuf).set_aspect(2, 3)
     prepare_pixbuf
     GC.start
   end
 
   def prepare_pixbuf
     if @pixbuf
-      scale_factor = compute_scale(@image, @pixbuf)
+      scale_factor = compute_scale(@image, @crop)
       scale_pixbuf(scale_factor)
+      @scaled_crop = @crop.scale(scale_factor)
     else
       @scaled_pixbuf = nil
     end
+  end
+
+  def compute_scale(widget, crop)
+    displayed_width = widget.allocated_width - 2*FRAME_PIXELS
+    width_scale = displayed_width.to_f / crop.width
+
+    displayed_height = widget.allocated_height - 2*FRAME_PIXELS
+    height_scale = displayed_height.to_f / crop.height
+
+    scale = width_scale < height_scale ? width_scale : height_scale
+    scale > 1 ? 1 : scale
   end
 
   def scale_pixbuf(scale)
@@ -64,19 +81,6 @@ class PhotoWindow
     end
   end
 
-  def compute_scale(image, pixbuf)
-    image_width = image.allocated_width
-    pixbuf_width = pixbuf.width
-    width_scale = image_width.to_f / pixbuf_width
-
-    image_height = image.allocated_height
-    pixbuf_height = pixbuf.height
-    height_scale = image_height.to_f / pixbuf_height
-
-    scale = width_scale < height_scale ? width_scale : height_scale
-    scale > 1 ? 1 : scale
-  end
-
   # This is only for packing the window layout, yuck.
   #
   def get_widget
@@ -85,31 +89,36 @@ class PhotoWindow
 
   def draw(widget, cr)
     if @scaled_pixbuf
-      frame_width = 20
+
+      cropped_pixbuf = @scaled_pixbuf.new_subpixbuf(
+        @scaled_crop.left, @scaled_crop.top,
+        @scaled_crop.width, @scaled_crop.height)
 
       width = widget.allocated_width
       height = widget.allocated_height
 
-      x = (width - @scaled_pixbuf.width) / 2
-      y = (height - @scaled_pixbuf.height) / 2
+      # (x,y) are upper left widget coordinate for the cropped image.
 
-      cr.set_source_pixbuf(@scaled_pixbuf, x, y)
-      cr.rectangle(x, y, @scaled_pixbuf.width, @scaled_pixbuf.height)
+      x = (width - cropped_pixbuf.width) / 2 + FRAME_PIXELS
+      y = (height - cropped_pixbuf.height) / 2 + FRAME_PIXELS
+
+      cr.set_source_pixbuf(cropped_pixbuf, x, y)
+      cr.rectangle(x, y, cropped_pixbuf.width, cropped_pixbuf.height)
       cr.fill
 
       cr.set_source_rgba(0, 0, 0, 1.0)
 
       # Left
-      cr.rectangle(0, 0, x + frame_width, height)
+      cr.rectangle(0, 0, x, height)
 
       # Right
-      cr.rectangle(width - (x + frame_width), 0, x + frame_width, height)
+      cr.rectangle(width - x, 0, width, height)
 
       # Top
-      cr.rectangle(0, 0, width, y + frame_width)
+      cr.rectangle(0, 0, width, y)
 
       # Bottom
-      cr.rectangle(0, height - (y + frame_width), width, y + frame_width)
+      cr.rectangle(0, height - y, width, height)
 
       cr.fill
     end
@@ -124,11 +133,14 @@ end
 # Try image coordinates.
 
 class Crop
+  attr_reader :left, :top, :width, :height
+
   def self.for_pixbuf(pixbuf)
-    new(0, 0, pixbuf.width, pixbuf.height)
+    new(pixbuf, 0, 0, pixbuf.width, pixbuf.height)
   end
 
-  def initialize(left, top, width, height)
+  def initialize(pixbuf, left, top, width, height)
+    @pixbuf = pixbuf
     @left = left
     @top = top
     @width = width
@@ -174,6 +186,22 @@ class Crop
     left = bound(left, 0, @pixbuf.width - width)
     top = bound(top, 0, @pixbuf.height - height)
 
-    Crop.new(left, top, width, height)
+    Crop.new(@pixbuf, left, top, width, height)
+  end
+
+  def bound(val, min, max)
+    case
+    when val < min
+      min
+    when val > max
+      max
+    else
+      val
+    end
+  end
+
+  def scale(factor)
+    Crop.new(
+      nil, @left * factor, @top * factor, @width * factor, @height * factor)
   end
 end
